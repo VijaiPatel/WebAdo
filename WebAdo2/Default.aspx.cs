82186fad53e4db3db7ff7e5b354c8ba7ec671868
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-06-30 13:57:14 +0100

    Add project files.

---
+++ WebAdo2/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+
+namespace WebAdo2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello World");
+            Response.Write("<br/>");
+
+            string s = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            Response.Write(s);
+        }
+    }
+
+}
---
+++ WebAdo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAdo2.Global" Language="C#" %>
